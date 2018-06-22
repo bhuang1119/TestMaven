b34199b97033c4c995a44f6d3602bf11fcfc22b0
huangbin <huangbin@souche.com>
2018-06-22 10:31:33 +0800

    init

---
+++ src/main/java/com/test/IntrospectorDemo.java
@@ -0,0 +1,45 @@
+package com.test;
+
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+
+/**
+ * @Author: huangbin
+ * @Description:
+ * @Date: Created in
+ * @Modified By:
+ */
+public class IntrospectorDemo {
+    String name;
+    String age;
+    public static void main(String[]args) throws Exception{
+        IntrospectorDemo demo=new IntrospectorDemo();
+        demo.setName("WinterLau");
+        demo.setAge("12");
+        //如果不想把父类的属性也列出来的话，
+        //那getBeanInfo的第二个参数填写父类的信息
+        BeanInfo bi=Introspector.getBeanInfo(demo.getClass(),Object.class);
+        PropertyDescriptor[] props=bi.getPropertyDescriptors();
+        for(int i=0;i<props.length;i++){
+            System.out.println(props[i].getName()+"="+ props[i].getReadMethod().invoke(demo,null));
+        }
+
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/com/test/ManygeSystem.java
@@ -0,0 +1,23 @@
+package com.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @Author: huangbin
+ * @Description:
+ * @Date: Created in
+ * @Modified By:
+ */
+@Controller
+public class ManygeSystem {
+    @RequestMapping("/Login")
+    public ModelAndView login(@RequestParam("username") String userName,
+                              @RequestParam("password") String passWord) {
+        ModelAndView mv = new ModelAndView("menu");//指定视图
+        mv.addObject("name", userName);
+        return mv;
+    }
+}
